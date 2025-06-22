b4696e2121335774f039f97f67de24c6386f4191
ProximaCA <denis-platov2003@yandex.ru>
2025-06-23 00:58:17 +0300

    Updates

---
+++ _next/static/aAKTAX0ESPLqUnJxc1s6a/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-4ba85e0b19cbbbb1.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
